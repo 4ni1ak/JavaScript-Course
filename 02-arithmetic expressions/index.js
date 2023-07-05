3c0fa78378ee370918b3db4de58d9bfd27eeefdd
Anıl Akpınar <4ni1ak+github@gmail.com>
2023-07-05 11:48:46 +0300

    arithmetic expressions added

---
+++ 02-arithmetic expressions/index.js
@@ -0,0 +1,24 @@
+/*
+    arithmetic expression is a combination of...
+    operands (values, variables, etc.)
+    operators (+ - * / %)
+    that can be evaluated to a value
+    ex. y = x + 5
+*/
+
+let students = 20;
+
+//students = students + 1;
+//students = students 1;
+//students = students • 2;
+//students students / 2;
+//let extraStudents = students % 2;
+
+//students += 1;
+//students -= 1;
+//students = 2;
+//students l = 2
+let result = 1+2*(3+4);
+console.log(result)
+
+ 
