c3694a27e9778e4d547dfdc1b7c26f9440edab3d
Anıl Akpınar <4ni1ak+github@gmail.com>
2023-07-07 09:57:19 +0300

    type conversion added

---
+++ 04-type conversion/index.js
@@ -0,0 +1,13 @@
+let age = window.prompt("how old are u ?");
+console.log(typeof age);
+age= Number(age);
+age += 1;
+console.log("Happy Birthday! U are ",age,"years old" );
+
+let x,y,z;
+x= Number("3.14");
+y=String(x);
+z=Boolean("doner");// u can use user input check
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
