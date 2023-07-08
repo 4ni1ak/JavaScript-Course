79fb4cab2dbe43cc72194213037c520e7e0ad35d
Anıl Akpınar <4ni1ak+github@gmail.com>
2023-07-08 10:53:44 +0300

    const added

---
+++ 05-const/index.js
@@ -0,0 +1,17 @@
+//const cant be changed
+const PI=3.14159
+let radius;
+let circuference;
+
+radius= window.prompt("Enter the radious of a circle");
+radius = Number(radius);
+
+circuference=2*PI*radius;
+console.log("The circuference is : ",circuference);
+
+
+
+
+
+
+
