db0516c7070e05539bf35f15c09f4e73e83aaef2
Anıl Akpınar <4ni1ak+github@gmail.com>
2023-07-09 17:44:12 +0300

    Math added

---
+++ 06-Math/index.js
@@ -0,0 +1,14 @@
+let a;
+let b;
+let c ;
+
+//x = Math.round(x);
+//x = Math.floor(x);
+//x = Math.ceil(x);
+//x= Math.pow(x, 2);
+//x= Math.sqrt(x);
+//x = Math.abs(x); //maximum = Math.max(x, y, z);
+//minimum = Math.min(x, y, z);
+x = Math.PI;
+
+console.log(x);
