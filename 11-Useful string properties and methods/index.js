8a2480d5ed8c962e37422a0848659124cb32a444
Anıl Akpınar <4ni1ak+github@gmail.com>
2023-07-14 00:38:25 +0300

    Useful string properties and methods added

---
+++ 11-Useful string properties and methods/index.js
@@ -0,0 +1,15 @@
+let userName = "4ni1ak";
+let phoneNumber = "123-456-7890";
+
+//console.log(userName.length);
+//console.log(userName.charAt(0));
+//console.log(userName.indexOf("o"));
+//console.log(userName.lastIndexOf("o"));
+//userName = userName.trim();
+//userName = userName.toUpperCase();
+//userName = userName.toLowerCase();
+
+phoneNumber = phoneNumber.replaceAll("-", "");
+
+console.log(phoneNumber);
+
