f5c19b01e6abe08b8510e202aaa8f49f8e965459
Anıl Akpınar <4ni1ak+github@gmail.com>
2023-07-17 10:33:17 +0300

    if statements added

---
+++ 14-If statements/index.js
@@ -0,0 +1,21 @@
+let temperature = 25;
+
+if (temperature >= 30) {
+  console.log("It's hot outside!");
+} else if (temperature >= 20) {
+  console.log("It's warm outside!");
+} else if (temperature >= 10) {
+  console.log("It's cool outside!");
+} else {
+  console.log("It's cold outside!");
+}
+/*
+let online = false;
+
+if(online){
+    console.log("You are online!");
+}
+else{
+    console.log("You are offline!");
+}
+*/
