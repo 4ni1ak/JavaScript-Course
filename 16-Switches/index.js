634184fbb290e8241820f0a03aca5956d90c750a
Anıl Akpınar <4ni1ak+github@gmail.com>
2023-07-19 17:46:54 +0300

    Switches added

---
+++ 16-Switches/index.js
@@ -0,0 +1,56 @@
+let grade = "A";
+
+switch(grade){
+ 
+    case "A":
+      console.log("You did great!");
+      break;
+   
+    case "B":
+      console.log("You did good!");
+      break;
+ 
+    case "C":
+      console.log("You did okay!");
+      break;
+ 
+    case "D":
+      console.log("You passed ... barely");
+      break;
+ 
+    case "F":
+      console.log("YOU FAILED!");
+      break;
+ 
+    default:
+      console.log(grade, "is not a letter grade");
+}
+
+/*
+let grade = 95;
+
+switch(true){
+
+    case grade >= 90:
+        console.log("You did great!");
+        break;
+
+    case grade >= 80:
+        console.log("You did good!");
+        break;
+
+    case grade >= 70:
+        console.log("You did okay!");
+        break;
+
+    case grade >= 60:
+        console.log("You passed ... barely");
+        break;
+
+    case grade > 60:
+        console.log("You FAILED!");
+        break;
+
+    default:
+        console.log(grade, "is not a letter grade!");
+}*/
