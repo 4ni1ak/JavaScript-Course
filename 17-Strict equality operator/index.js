087db1b6979a0c53ab7ac7a69a8caeafc800b26b
Anıl Akpınar <4ni1ak+github@gmail.com>
2023-07-20 10:34:04 +0300

    Strict equality operator added

---
+++ 17-Strict equality operator/index.js
@@ -0,0 +1,12 @@
+// = assignment operator
+// == comparison operator
+// === strict equality operator
+
+let x = "3.14";
+
+if(x === 3.14){
+    console.log("That is pi");
+}
+else{
+    console.log("That is NOT pi");
+}
