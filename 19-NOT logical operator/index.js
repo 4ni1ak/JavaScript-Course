8fd82ae18dc406e1d1d76b8a639f1aa9d8e25540
Anıl Akpınar <4ni1ak+github@gmail.com>
2023-07-23 17:37:22 +0300

    NOT logical operator added

---
+++ 19-NOT logical operator/index.js
@@ -0,0 +1,16 @@
+let temp = 15;
+let sunny = false;
+
+if(!(temp > 0)){
+    console.log("It's cold outside");
+}
+else{
+    console.log("It's warm outside");
+}
+
+if(!sunny){
+    console.log("It's cloudy outside");
+}
+else{
+    console.log("It's sunny outside");
+}
