9f6151eddb3b4cabebc9b1287fd5134c049cbdd9
Anıl Akpınar <4ni1ak+github@gmail.com>
2023-07-29 21:41:24 +0300

    Functions added

---
+++ 25-Functions/index.js
@@ -0,0 +1,16 @@
+startProgram();
+
+function startProgram(){
+    let userName = "anil";
+    let age = 20;
+    
+    happyBirthday(userName, age);
+}
+
+function happyBirthday(userName, age){
+    console.log("Happy birthday to you!");
+    console.log("Happy birthday to you!");
+    console.log("Happy birthday dear", userName);
+    console.log("Happy birthday to you!");
+    console.log("You are", age,"years old!");
+}
