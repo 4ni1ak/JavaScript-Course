706c9cb2a599fae7791a5385e54625d65b3332ae
Anıl Akpınar <4ni1ak+github@gmail.com>
2023-07-30 10:55:17 +0300

    Return statement added

---
+++ 26-Return statement/index.js
@@ -0,0 +1,14 @@
+let area;
+let width;
+let height;
+
+width = window.prompt("Enter width");
+height = window.prompt("Enter height");
+
+area = getArea(width, height);
+
+console.log("The area is:", area);
+
+function getArea(width, height){
+    return width * height;
+}
