dbd864a2d2cc229821d3121bc26904179c9d98a0
Anıl Akpınar <4ni1ak+github@gmail.com>
2023-07-31 23:54:41 +0300

    Ternary operator added

---
+++ 27-Ternary operator/index.js
@@ -0,0 +1,15 @@
+let adult = checkAge(12);
+console.log(adult);
+ 
+function checkAge(age){
+ 
+    return age >= 18? true : false;
+}
+/*
+checkWinner(false);
+ 
+function checkWinner(win){
+ 
+    win ? console.log("You win!") : console.log("You lose!");
+}
+*/
