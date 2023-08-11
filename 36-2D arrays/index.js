9cf85f1aaafe6343b99777dcc71503dfe1791df4
Anıl Akpınar <4ni1ak+github@gmail.com>
2023-08-11 23:55:06 +0300

    2D arrays added

---
+++ 36-2D arrays/index.js
@@ -0,0 +1,13 @@
+let fruits =           ["apples", "oranges", "bananas"];
+let vegetables = ["carrots", "onions", "potatoes"];
+let meats =         ["eggs", "chicken", "fish"];
+
+let groceryList = [fruits, vegetables, meats];
+
+groceryList[2][2] = "steak";
+
+for(let list of groceryList){
+    for(let food of list){
+        console.log(food);
+    }
+}
