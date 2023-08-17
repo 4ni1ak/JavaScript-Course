df62a8b04667f0778acef789207f2dcbba6fcbbc
Anıl Akpınar <4ni1ak+github@gmail.com>
2023-08-17 13:29:51 +0300

    Array.filter added

---
+++ 42-Array.filter()/index.js
@@ -0,0 +1,11 @@
+let ages = [18, 16, 21, 17, 19, 90];
+let adults = ages.filter(checkAge);
+
+adults.forEach(print);
+
+function checkAge(element){
+    return element >= 18;
+}
+function print(element){
+    console.log(element);
+}
