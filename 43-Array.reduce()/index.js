9c58e56bd6d8910db5129ee152be3986832be2cf
Anıl Akpınar <4ni1ak+github@gmail.com>
2023-08-18 16:25:55 +0300

    Array.reduce added

---
+++ 43-Array.reduce()/index.js
@@ -0,0 +1,8 @@
+let prices = [5, 10, 15, 20, 25, 30];
+let total = prices.reduce(checkOut);
+
+console.log(`The total is: $${total}`);
+
+function checkOut(total, element){
+    return total + element;
+}
